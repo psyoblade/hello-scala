b88d198164543e4d66c482c9964021f541cde4f7
psyoblade <park.suhyuk@gmail.com>
2016-07-17 17:08:58 +0000

    add hello

---
+++ helloworld.scala
@@ -0,0 +1,12 @@
+var xxx = "helloworld"
+println(xxx)
+
+// 시퀀스의 출력이 간단함.
+(0 to 10).foreach(print)
+println
+
+// 함수와 변수의 선언이 아주 유사함.
+var varInt:Int = 30
+def defInt:Int = 30
+println(varInt)
+println(defInt)
